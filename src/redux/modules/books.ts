63de9e721e5e22fd7a372ccd80f9d31dcad435ec
sjpark <sjpark@unison.co.kr>
2020-08-04 10:07:17 +0900

    #Action type  선언 부분 코드 줄이기

--- src/redux/modules/books.ts
+++ src/redux/modules/books.ts
@@ -18,41 +18,19 @@ const initialState: BooksState = {
   error: null,
 };
 
-export const GET_BOOKS_LIST = 'books/GET_BOOKS_LIST';
-export const GET_BOOKS_LIST_SUCCESS = 'books/GET_BOOKS_LIST_SUCCESS';
-export const GET_BOOKS_LIST_ERROR = 'books/GET_BOOKS_LIST_ERROR';
-
-export const ADD_BOOK = 'books/ADD_BOOK';
-export const ADD_BOOK_SUCCESS = 'books/ADD_BOOK_SUCCESS';
-export const ADD_BOOK_ERROR = 'books/ADD_BOOK_ERROR';
-
-export const EDIT_BOOK = 'books/EDIT_BOOK';
-export const EDIT_BOOK_SUCCESS = 'books/EDIT_BOOK_SUCCESS';
-export const EDIT_BOOK_ERROR = 'books/EDIT_BOOK_ERROR';
-
-export const DETAIL_BOOK = 'books/DETAIL_BOOK';
-export const DETAIL_BOOK_SUCCESS = 'books/DETAIL_BOOK_SUCCESS';
-export const DETAIL_BOOK_ERROR = 'books/DETAIL_BOOK_ERROR';
-
-export const DELETE_BOOK = 'books/DELETE_BOOK';
-export const DELETE_BOOK_SUCCESS = 'books/DELETE_BOOK_SUCCESS';
-export const DELETE_BOOK_ERROR = 'books/DELETE_BOOK_ERROR';
-
-export const PENDING = 'books/PENDING';
-
-export const pending = createAction(PENDING)();
-export const list = createAction(GET_BOOKS_LIST)();
-export const listSuccess = createAction(GET_BOOKS_LIST_SUCCESS)<BookResType[]>();
-export const listError = createAction(GET_BOOKS_LIST_ERROR)<Error>();
-export const add = createAction(ADD_BOOK)<BookReqType>();
-export const addSuccess = createAction(ADD_BOOK_SUCCESS)<BookResType>();
-export const addError = createAction(ADD_BOOK_ERROR)<Error>();
-export const edit = createAction(EDIT_BOOK)<BookReqType, number>();
-export const editSuccess = createAction(EDIT_BOOK_SUCCESS)<BookResType>();
-export const editError = createAction(EDIT_BOOK_ERROR)<Error>();
-export const deleteBook = createAction(DELETE_BOOK)<number>();
-export const deleteBookSuccess = createAction(DELETE_BOOK_SUCCESS)<number>();
-export const deleteBookError = createAction(DELETE_BOOK_ERROR)<Error>();
+export const pending = createAction('PENDING')();
+export const list = createAction('GET_BOOKS_LIST')();
+export const listSuccess = createAction('GET_BOOKS_LIST_SUCCESS')<BookResType[]>();
+export const listError = createAction('GET_BOOKS_LIST_ERROR')<Error>();
+export const add = createAction('ADD_BOOK')<BookReqType>();
+export const addSuccess = createAction('ADD_BOOK_SUCCESS')<BookResType>();
+export const addError = createAction('ADD_BOOK_ERROR')<Error>();
+export const edit = createAction('EDIT_BOOK')<BookReqType, number>();
+export const editSuccess = createAction('EDIT_BOOK_SUCCESS')<BookResType>();
+export const editError = createAction('EDIT_BOOK_ERROR')<Error>();
+export const deleteBook = createAction('DELETE_BOOK')<number>();
+export const deleteBookSuccess = createAction('DELETE_BOOK_SUCCESS')<number>();
+export const deleteBookError = createAction('DELETE_BOOK_ERROR')<Error>();
 
 const bookActions = { 
   pending
@@ -184,8 +162,8 @@ function* editBookSaga(action: EditAction) {
 
 // [project] saga 함수를 실행하는 액션과 액션 생성 함수를 작성했다.
 export function* sagas() {
-  yield takeEvery(GET_BOOKS_LIST, getBooksSaga);
-  yield takeEvery(ADD_BOOK, addBookSaga);
-  yield takeEvery(DELETE_BOOK, deleteBookSaga);
-  yield takeEvery(EDIT_BOOK, editBookSaga);
+  yield takeEvery('GET_BOOKS_LIST', getBooksSaga);
+  yield takeEvery('ADD_BOOK', addBookSaga);
+  yield takeEvery('DELETE_BOOK', deleteBookSaga);
+  yield takeEvery('EDIT_BOOK', editBookSaga);
 }
